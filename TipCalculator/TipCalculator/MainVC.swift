9f4de389983809f70b0a98c5d3b89fc09431e022
arvlos <artemvlosev@gmail.com>
2016-03-25 16:55:37 -0400

    Initial commit

---
+++ TipCalculator/TipCalculator/MainVC.swift
@@ -0,0 +1,13 @@
+//
+//  MainVC.swift
+//  TipCalculator
+//
+//  Created by Artem Losev on 3/25/16.
+//  Copyright © 2016 Artem Losev. All rights reserved.
+//
+
+import Cocoa
+
+class MainVC: UIViewController {
+
+}
